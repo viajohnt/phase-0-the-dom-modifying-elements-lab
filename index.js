79dd1b25eb83a3b78a464b0ae7d936e3ef59cd52
John Truong <viajohnt@gmail.com>
2023-03-22 15:27:13 -0600

    Final Commit

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.querySelector('main#main').remove();
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'John Truong is the champion';
+
+
